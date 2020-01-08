dd8a2c032de981b13e7b907d8d3b199cf8c95b58
JohnXW <wongkahingerv@gmail.com>
2020-01-08 18:02:39 +0800

    update

--- Assets/TFT/Heros/Scripts/Hero.cs
+++ Assets/TFT/Heros/Scripts/Hero.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Hero : MonoBehaviour
 {
     public bool isEnemy;
+    public HeroPlace HeroPlace;
     public MouseSelect MouseSelect;
     public HeroStatus HeroStatus;
     public Rarity Rarity;
