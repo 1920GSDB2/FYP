77b8780956fa26617f5bc5ca93c47dbd042a5e80
JohnXW <wongkahingerv@gmail.com>
2020-01-12 04:18:46 +0800

    update

---
+++ Assets/LobbyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviour
+{
+    public Button StartButton, LobbyButton, InformationButton;
+    public GameObject StartPanel, LobbyPanel, InformationPanel;
+    public Button MatchButton, CreateCustomButton, JoinCustomButton;
+    public GameObject MatchModePanel, CreateCustomPanel, JoinCustomPanel;
+    public Button OkayButton, BackLobbyButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
