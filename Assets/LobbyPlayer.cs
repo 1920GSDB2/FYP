a3a15951b6968e1f8f1e81ae51932a4db15c7447
JohnXW <wongkahingerv@gmail.com>
2020-01-13 01:21:58 +0800

    Revert ".2"
    This reverts commit ae918078ad8826614cd80e5c60eaaa62849a8772.

---
+++ Assets/LobbyPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LobbyPlayer : MonoBehaviour
+{
+    public TextMeshProUGUI PlayerLevel;
+    public Image PlayerIcon;
+    public TextMeshProUGUI PlayerName;
+    public Button ReadyButton;
+    public Button KickButton;
+
+    bool isLocal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocal) KickButton.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
