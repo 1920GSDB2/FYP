30528ccc9cb9bf47651520c96433d46d560a23b6
JohnXW <wongkahingerv@gmail.com>
2020-01-28 01:35:35 +0800

    GAME BOARD

---
+++ Assets/PlayerArena.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArena : MonoBehaviour
+{
+    public GameObject FightArena;
+    public GameObject SelfArena, EnemyArena;
+    Vector3 leftTop, rightTop, leftDown, rightDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCorner();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetCorner()
+    {
+        Vector3 boundExtents = FightArena.GetComponent<Renderer>().bounds.extents * 2;
+        leftTop = FightArena.transform.position;
+        leftDown = new Vector3(leftTop.x, leftTop.y, leftTop.z + boundExtents.z);
+        rightTop = new Vector3(leftTop.x + boundExtents.x, leftTop.y, leftTop.z);
+        rightDown = new Vector3(rightTop.x + boundExtents.x, rightTop.y, rightTop.z);
+    }
+}
