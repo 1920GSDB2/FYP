aab3e7c8e74ab4396b51440c9627bf017606e66c
JohnXW <wongkahingerv@gmail.com>
2020-03-21 16:48:41 +0800

    BuffList UI

--- Assets/TFT/UI/Scripts/BuffList.cs
+++ Assets/TFT/UI/Scripts/BuffList.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BuffList : MonoBehaviour
@@ -8,16 +9,27 @@ public class BuffList : MonoBehaviour
     public Buff buff;
     public static BuffList Instance;
 
+    public TextMeshProUGUI ButtonText;
+    public float delayTime = 0.2f;
+
+    private bool isShowList;
+    private Vector2 currPos, nextPos;
+    private RectTransform RectTransform;
+
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
+        RectTransform = GetComponent<RectTransform>();
+        currPos = RectTransform.anchoredPosition;
+        nextPos = RectTransform.anchoredPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RectTransform.anchoredPosition = Vector2.Lerp(currPos, nextPos, delayTime);
+        currPos = RectTransform.anchoredPosition;
     }
 
     public void ClearBuff()
@@ -39,8 +51,15 @@ public class BuffList : MonoBehaviour
         newBuff.name = name;
         //newBuff.gameObject.transform.parent = buffList;
         newBuff.gameObject.transform.SetParent(buffList);
-        newBuff.gameObject.GetComponent<RectTransform>().localScale = Vector3.one;
-        buffList.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(75, 24 * buffList.gameObject.transform.childCount);
+        //newBuff.gameObject.GetComponent<RectTransform>().localScale = Vector3.one;
+        //buffList.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(75, 24 * buffList.gameObject.transform.childCount);
+        if (!isShowList)
+        {
+            RectTransform.anchoredPosition = new Vector2(-RectTransform.sizeDelta.x + 10, -115);
+        }
+
+        currPos = RectTransform.anchoredPosition;
+        nextPos = RectTransform.anchoredPosition;
     }
 
     public void UpgradeBuff(string buffer)
@@ -48,5 +67,21 @@ public class BuffList : MonoBehaviour
         Transform upgradeObject = buffList.Find(buffer);
         if (upgradeObject != null)
             upgradeObject.gameObject.GetComponent<Buff>().CurrentValue++;
+    }
+
+    public void SwitchPanel()
+    {
+        if (RectTransform.sizeDelta.x <= 10) return;
+        isShowList = !isShowList;
+        if (isShowList)
+        {
+            nextPos.x = 0;
+            ButtonText.text = "<";
+        }
+        else
+        {
+            nextPos.x = -RectTransform.sizeDelta.x + 10;
+            ButtonText.text = ">";
+        }
     }
 }
