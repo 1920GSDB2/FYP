c2e4ddf2846a977b3edc69cc52fb67d40d55e3c7
afafafafawrh <59306764+afafafafawrh@users.noreply.github.com>
2020-05-23 23:28:46 +0800

    new fix

--- Assets/TFT/Script/Hero/FallenVirago.cs
+++ Assets/TFT/Script/Hero/FallenVirago.cs
@@ -1,15 +1,15 @@
-﻿
+﻿using UnityEngine;
 public class FallenVirago : Hero
 {
     float tempHealth;
     float tempMaxHp;
     public override void targetDie()
     {
-        base.targetDie();
-        tempHealth += 300;
-        tempMaxHp += 300;
+        base.targetDie();      
         if (!isMirror)
         {
+            tempHealth += 300;
+            tempMaxHp += 300;
             photonView.RPC("RPC_SyncHeroAttribute", PhotonTargets.All, (byte)HeroAttribute.maxHp, MaxHealth + 300);
             photonView.RPC("RPC_Heal", PhotonTargets.All, 300f, (byte)DamageType.Heal);
         }
@@ -19,5 +19,7 @@ public class FallenVirago : Hero
     {
         base.ResetStatus();
         photonView.RPC("RPC_SyncHeroAttribute", PhotonTargets.All, (byte)HeroAttribute.maxHp, MaxHealth -tempHealth);
+        Debug.Log("maxHealth" + MaxHealth + "temp " + tempHealth);
+        tempHealth = 0;      
     }
 }
