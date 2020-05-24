b03788bb5bd64fab1fbcbd38dbc2ee7d85d36397
JohnXW <wongkahingerv@gmail.com>
2020-05-24 09:46:32 +0800

    Test DB

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,24 +1,79 @@
-﻿using System.Collections;
+﻿using GoogleSheetsToUnity;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct TestFriend
+{
+    public string name;
+    public string[] friends;
+}
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Animation animation;
+    [HideInInspector]
+    public string associatedSheet = "1lPHnbJSsiZY_cFtD9HwBQ9wMSsTeYTRMh2un-T6shC8";
+    [HideInInspector]
+    public string associatedWorksheet = "test";
+
+    List<string> list;
     void Start()
     {
-        
+        //List<string> list1 = new List<string>{
+        //    "Test1",
+        //    "Test2",
+        //    "Test3",
+        //    "Test4",
+        //    "Test5",
+        //    "Test6",
+        //};
+        //List<string> list2 = new List<string>{
+        //    "Test5",
+        //    "Test6",
+        //    "Test7",
+        //    "Test8",
+        //    "Test9",
+        //    "Test10",
+        //};
+        //List<List<string>> combineList = new List<List<string>>{
+        //    list1,
+        //    list2
+        //};
+        //TestFriend test = new TestFriend();
+        //test.name = "ABC";
+        //test.friends = list1.ToArray();
+        //List<string> list3 = new List<string>{
+        //    JsonUtility.ToJson(test),
+        //};
+        //JsonUtility.ToJson(list1);
+        //SpreadsheetManager.Append(new GSTU_Search(associatedSheet, associatedWorksheet), new ValueRange(list3), null);
+        UpdateHandler();
+
+    }
+    private void UpdateHandler()
+    {
+        SpreadsheetManager.Read(new GSTU_Search(associatedSheet, associatedWorksheet), UpdateTest);
     }
 
+    private void UpdateTest(GstuSpreadSheet ss)
+    {
+        List<string> list2 = new List<string>{
+            "Test5",
+            "Test6",
+            "Test7",
+            "Test8",
+            "Test9",
+            "Test10",
+        };
+        TestFriend test = new TestFriend();
+
+        test.name = "Bdd";
+        test.friends = list2.ToArray();
+        ss["Bdd", "data"].UpdateCellValue(associatedSheet, associatedWorksheet, JsonUtility.ToJson(test));
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K)) {
-            
-                animation.Play("3HitComboWeapon");
-           // animation.Stop();
-            
-        }
+        
     }
 }
