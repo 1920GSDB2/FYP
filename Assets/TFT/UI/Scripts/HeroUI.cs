33f3515c9e45bbadee0acf148b3f4b6945671646
JohnXW <wongkahingerv@gmail.com>
2020-05-25 04:50:18 +0800

    Shop ICON

--- Assets/TFT/UI/Scripts/HeroUI.cs
+++ Assets/TFT/UI/Scripts/HeroUI.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class HeroUI : MonoBehaviour
 {
@@ -8,6 +10,7 @@ public class HeroUI : MonoBehaviour
     public HeroRace[] HeroRares;
     public BasicInfo BasicInfo;
     public Hero Hero;
+    public Image HeroIcon;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,9 @@ public class HeroUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Hero != null)
+        {
+            HeroIcon.sprite = Hero.Icon;
+        }
     }
 }
